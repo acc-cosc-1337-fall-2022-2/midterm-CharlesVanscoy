88e5b900c549037c41ae583b89aefed1b6087b2e
CharlesVanscoy <charles.vanscoy@g.austincc.edu>
2022-10-16 01:14:20 -0500

    Midterm Commit

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -2,8 +2,24 @@
 #include "question4.h"
 int main()
 {
-    int in;
-    cin >> in;
-    get_fib_sequence(in);
+    bool exit;
+    char choice;
+    int in = 0;
+    do{
+        do {
+            cout << "\nEnter an integer n from 1 to 15 to display n Fibonacci iterations\n";
+            cin >> in;
+            if (in < 1 || in > 15) {
+                cout << "\nINVALID INPUT TRY AGAIN\n";
+            }
+        } while (in < 1 || in > 15);
+        string sequence;
+        sequence = get_fib_sequence(in);
+        cout << sequence << "\n";
+        cout << "\nDo you want to go again? (y/n)\n";
+        cin >> choice;
+        if (choice == 'n')
+            exit = true;
+    } while (exit !=true);
     return 0;
 }
--- src/question_4/question4.cpp
+++ src/question_4/question4.cpp
@@ -7,22 +7,27 @@ bool test_config()
 }
 
 string get_fib_sequence(int input){
-    string sequence;
-    int i = 0;
+    string sequence = "0 1 ";
+    int i = 1;
     int n1 = 0;
     int n2 = 1;
     int n3;
     string conv;
+    
     while (i < input){
         n3 = n1 + n2;
-        conv = n3;
+        if (i == input-1){
+            conv = std::to_string(n3);
+        }
+        else {
+            conv = std::to_string(n3) + ' ';
+        }
         n1 = n2;
         n2 = n3;
         sequence.append(conv);
         i++;
     }
-    cout << sequence;
-    return 0;
+    return sequence;
 }
 
 // 0 1 1 2 3 5 8 13 21 34 55
